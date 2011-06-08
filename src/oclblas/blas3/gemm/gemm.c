89420494408309c9130d95fb26d76b50933a6a18
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2011-06-08 13:06:04 +0200

    - minor changes

--- src/oclblas/blas3/gemm/gemm.c
+++ src/oclblas/blas3/gemm/gemm.c
@@ -34,7 +34,6 @@ OpenCLStatus opencl_sgemm(const OCLBMatrixOrder order,
 
 	char definitions[256];
 	sprintf(definitions, "#define SUBMATRIX_SIZE %i\n", SUBMATRIX_SIZE);
-	fprintf(stdout, "%s", definitions);
 
 	const size_t sizes[] =
 	{ strlen(definitions), strlen(opencl_types), strlen(opencl_sgemm_kernel) };
@@ -160,177 +159,7 @@ OpenCLStatus opencl_dgemm(const OCLBMatrixOrder order,
 		const int ldc)
 {
 #if OCL_DOUBLE_SUPPORTED == 1
-	cl_int ret;
-
-	/* Create Memory Buffer */
-	cl_mem memA = clCreateBuffer(usedContext, CL_MEM_READ_WRITE, lda * k
-			* sizeof(float), NULL, &ret);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not create buffer for A!\n");
-		return FAILURE;
-	}
-	cl_mem memB = clCreateBuffer(usedContext, CL_MEM_READ_WRITE, ldb * n
-			* sizeof(float), NULL, &ret);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not create buffer for B!\n");
-		return FAILURE;
-	}
-	cl_mem memC = clCreateBuffer(usedContext, CL_MEM_READ_WRITE, ldc * n
-			* sizeof(float), NULL, &ret);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not create buffer for C!\n");
-		return FAILURE;
-	}
-
-	ret = clEnqueueWriteBuffer(usedCommandQueue, memA, CL_TRUE, 0, lda * k
-			* sizeof(float), a, 0, NULL, NULL);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not write buffer for A!\n");
-		return FAILURE;
-	}
-	ret = clEnqueueWriteBuffer(usedCommandQueue, memB, CL_TRUE, 0, ldb * n
-			* sizeof(float), b, 0, NULL, NULL);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not write buffer for B!\n");
-		return FAILURE;
-	}
-	ret = clEnqueueWriteBuffer(usedCommandQueue, memC, CL_TRUE, 0, ldc * n
-			* sizeof(float), c, 0, NULL, NULL);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not write buffer for C!\n");
-		return FAILURE;
-	}
-
-	/* Set OpenCL Kernel Arguments */
-	int colMajor = order == ColumnMajor ? 1 : 0;
-	int transa = transposeA == NO ? 0 : 1;
-	int transb = transposeB == NO ? 0 : 1;
-	ret = clSetKernelArg(dgemmKernel, 0, sizeof(int), (void *) &colMajor);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set transpose A!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 1, sizeof(int), (void *) &transa);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set transpose A!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 2, sizeof(int), (void *) &transb);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set transpose B!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 3, sizeof(int), (void *) &m);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set m!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 4, sizeof(int), (void *) &n);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set n!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 5, sizeof(int), (void *) &k);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set k!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 6, sizeof(float), (void *) &alpha);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set alpha!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 7, sizeof(cl_mem), (void *) &memA);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set mem A!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 8, sizeof(int), (void *) &lda);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set lda!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 9, sizeof(cl_mem), (void *) &memB);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set mem B!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 10, sizeof(int), (void *) &ldb);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set ldb!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 11, sizeof(float), (void *) &beta);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set alpha!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 12, sizeof(cl_mem), (void *) &memC);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set mem C!\n");
-		return FAILURE;
-	}
-	ret = clSetKernelArg(dgemmKernel, 13, sizeof(int), (void *) &ldc);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not set ldc!\n");
-		return FAILURE;
-	}
-
-	/* Execute OpenCL Kernel */
-	size_t global_item_size[2];
-	global_item_size[0] = m % 8 == 0 ? m : (m / 8 + 1) * 8;
-	global_item_size[1] = n % 8 == 0 ? n : (n / 8 + 1) * 8;
-	size_t local_item_size[3];
-	local_item_size[0] = 8;
-	local_item_size[1] = 8;
-
-	ret = clEnqueueNDRangeKernel(usedCommandQueue, dgemmKernel, 2, NULL,
-			global_item_size, local_item_size, 0, NULL, NULL);
-	if (ret != 0)
-	{
-		fprintf(stderr, "Could not enqueue nd range kernel (%i). Abort!\n", ret);
-		return FAILURE;
-	}
-
-	/* Copy results from the memory buffer */
-	ret = clEnqueueReadBuffer(usedCommandQueue, memC, CL_TRUE, 0, ldc * n
-			* sizeof(float), c, 0, NULL, NULL);
-	if (ret != 0)
-	{
-		char str[64];
-		getErrorMessage(ret, 64, str);
-		fprintf(stderr, "Could not read buffer (%s). Abort!\n", str);
-		return FAILURE;
-	}
-
-	/* Finalization */
-	ret = clFlush(usedCommandQueue);
-	ret = clFinish(usedCommandQueue);
-	ret = clReleaseMemObject(memA);
-	ret = clReleaseMemObject(memB);
-	ret = clReleaseMemObject(memC);
-
-	return SUCCESS;
+	return NOT_IMPLEMENTED;
 #else
 	return ARCHITECTURE_MISMATCH;
 #endif
@@ -351,5 +180,9 @@ OpenCLStatus opencl_zgemm(const OCLBMatrixOrder order,
 		const int lda, void *b, const int ldb, const void *beta, void *c,
 		const int ldc)
 {
+#if OCL_DOUBLE_SUPPORTED == 1
 	return NOT_IMPLEMENTED;
+#else
+	return ARCHITECTURE_MISMATCH;
+#endif
 }
