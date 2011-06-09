3810cc6650a2b8c62345fb7a6230efee9e087807
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2011-06-09 14:41:20 +0200

    - added testing facilities for kernel testing (some definitions need to be made to meet the OpenCL specification and some helper macros are needed)

---
+++ test/testutils/oclkerneltest.h
@@ -0,0 +1,19 @@
+/*
+ * oclkerneltest.h
+ *
+ *  Created on: Jun 9, 2011
+ *      Author: ludwig
+ */
+
+#ifndef OCLKERNELTEST_H_
+#define OCLKERNELTEST_H_
+
+#include "opencl_dummys.h"
+
+#define check(expected, actual) \
+	if (expected != actual) { \
+		fprintf(stderr, "%s != %s in %s:%i!\n", #expected, #actual, __FILE__, __LINE__); \
+		exit(1); \
+	}
+
+#endif /* OCLKERNELTEST_H_ */
---
+++ test/testutils/opencl_dummys.h
@@ -0,0 +1,15 @@
+/*
+ * opencl_dummys.h
+ *
+ *  Created on: Jun 9, 2011
+ *      Author: ludwig
+ */
+
+#ifndef OPENCL_DUMMYS_H_
+#define OPENCL_DUMMYS_H_
+
+#define __global
+#define __local
+#define __private
+
+#endif /* OPENCL_DUMMYS_H_ */
